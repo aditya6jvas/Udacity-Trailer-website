4358bf0d9343afd5e1ceaaeeb4438d7f322d6560
Aditya jagarlapudi <adityajagarlapudi@gmail.com>
2017-06-23 23:49:30 +0530

    Changes to be made

--- mvie.py
+++
@@ -1,6 +0,0 @@
-class Movie:
-    def __init__(self, titl, story, image, link): # Constructor
-        self.title = titl # Movie title
-        self.sline = story # Movie story
-        self.poster_image_url = image # Movie Poster
-        self.trailer_youtube_url = link # Movie Trailer
